cfdb9aa4ed236c4e0fe553fdf668419815a5a485
motayamlove-commits <3.1087279e+08+motayamlove-commits@users.noreply.github.com>
2026-08-17 13:25:30 +0000

    فك ضغط ملف لوحة التحكم ورفع المحتويات

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase configuration provided by the user
+const firebaseConfig = {
+  apiKey: "AIzaSyBT3drctxZJONfCaEPhurqpFW9KmdVgGi0",
+  authDomain: "panel-33258.firebaseapp.com",
+  databaseURL: "https://panel-33258-default-rtdb.firebaseio.com",
+  projectId: "panel-33258",
+  storageBucket: "panel-33258.firebasestorage.app",
+  messagingSenderId: "987800222103",
+  appId: "1:987800222103:web:c55c410a4f1a8bfc77ecd2",
+  measurementId: "G-VVSQT8KXG8"
+};
+
+// Global variable to be accessed by other scripts if needed
+window.firebaseConfig = firebaseConfig;
+console.log("Firebase configuration loaded successfully.");
